52610421717717126db8515690c208af32fbd4df
calt08 <cesarlopez2808@gmail.com>
2020-07-16 21:09:14 -0400

    some testCases, accepts negative numbers

--- addition_test.py
+++ addition_test.py
@@ -12,6 +12,17 @@ class TestAdditionMethods(unittest.TestCase):
     def test_emptySumEquals0(self):
         self.assertEqual(addition(), 0)
 
+    def test_exceedMaxInt(self):
+        # Max int is 2147483647, but when its larger than that is converted to negative value by subtracting 4294967296
+        # So There is no limit for long integer literals apart from what can be stored in available memory.
+        self.assertEqual(addition(4294967296, 4294967296), 8589934592)
+
+    def test_2010and2010Equals4020(self):
+        self.assertEqual(addition(2010, 2010), 4020)
+
+    def test_2andMinus200EqualsMinus198(self):
+        self.assertEqual(addition(2, -200), -198)
+
 
 if __name__ == '__main__':
     unittest.main()
