ba5e80150da92451eb879b37a573908890bd458d
calt08 <cesarlopez2808@gmail.com>
2020-07-16 20:55:39 -0400

    third testcase: empty sum returns 0

--- addition_test.py
+++ addition_test.py
@@ -9,6 +9,9 @@ class TestAdditionMethods(unittest.TestCase):
     def test_2and2and2equals6(self):
         self.assertEqual(addition(2, 2, 2), 6)
 
+    def test_emptySumEquals0(self):
+        self.assertEqual(addition(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
