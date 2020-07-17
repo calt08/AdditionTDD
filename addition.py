3806020cd8034ae2802cd668e69f8a263d0a7ddd
calt08 <cesarlopez2808@gmail.com>
2020-07-16 21:30:47 -0400

    Now has visual interface that accepts command line arguments and allows you to also run the program

--- addition.py
+++ addition.py
@@ -1,6 +1,31 @@
-def addition(*args):
+import sys
+
+
+def addition(*args, **kargs):
     sum = 0
+
+    if "nums" in kargs:
+        for b in kargs["nums"]:
+            sum += b
+
     for a in args:
         sum += a
-
     return sum
+
+
+def main():
+    sys.argv.pop(0)
+    args = list(map(int, sys.argv))
+
+    if(args):
+        print(addition(nums=args))
+    else:
+        n = int(input("How many numbers you want to Sum: "))
+        nums = []
+        for i in range(n):
+            nums.append(int(input(f"num {i+1}: ")))
+
+        print(addition(nums=nums))
+
+
+main()
